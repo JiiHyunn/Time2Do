c6df522317e4c8d871d98ca11aab9ceca9815cba
sth0824 <sth0824@naver.com>
2024-08-04 13:53:51 +0900

    0736

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Main from './Main';
+import Main from './Main.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
