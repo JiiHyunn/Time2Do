1a0a8534a62fc69643285a2075564874d7332ece
JiiHyunn <gh8526@hanmail.net>
2024-08-04 14:42:16 +0900

    0728

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Main from './Main.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter >
       <Main />
     </BrowserRouter>
   </React.StrictMode>
